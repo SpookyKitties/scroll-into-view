64f585bcfd8d0be8b8ae5f3ffd499db849289656
Jared Parkinson <jared@parkinson.im>
2019-06-14 10:20:04 -0600

    everything hopefully works

--- src/scroll-into-view.ts
+++ src/scroll-into-view.ts
@@ -1,6 +1,29 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+export function scrollIntoView(selector: string, options: Options): void {
+  let doc: Document | undefined;
+  const timeout = options.timeout ? options.timeout : 0;
+  if (options.document !== undefined) {
+    doc = options.document;
+  } else if (document !== undefined) {
+    doc = document;
+  } else {
+    return;
+  }
 
+  try {
+    const element = doc.querySelector(selector);
+    if (element) {
+      setTimeout((): void => {
+        element.scrollIntoView();
+      }, timeout);
+    }
+  } catch (error) {}
+}
+
+export async function asyncScrollIntoView(selector: string, options: Options): Promise<void> {
+  scrollIntoView(selector, options);
+}
+
+class Options {
+  public document: Document | undefined;
+  public timeout: number | undefined;
 }
